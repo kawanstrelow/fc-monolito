342775334cc0ebd44859d9d301433df867918177
Kawan R. Strelow <kawanstrelow1@gmail.com>
2024-10-18 20:33:38 -0400

    upload

--- src/modules/@shared/domain/value-object/address.ts
+++ src/modules/@shared/domain/value-object/address.ts
@@ -46,9 +46,9 @@ export default class Address implements ValueObject {
     if (this._street.length === 0) {
       throw new Error("Street is required")
     }
-    if (this._number.length === 0) {
-      throw new Error("Number is required")
-    }
+    // if (this._number === 0) {
+    //   throw new Error("Number is required")
+    // }
     if (this._complement.length === 0) {
       throw new Error("Complement is required")
     }
